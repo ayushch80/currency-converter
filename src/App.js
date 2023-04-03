2260faaa6fa78de6eb1d4830999e732ed01e00bc
ayushch80 <ayushch80@gmail.com>
2023-04-03 11:31:32 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState(null);
+  const [amount, setAmount] = useState('');
+  const [fromCurrency, setFromCurrency] = useState('USD');
+  const [toCurrency, setToCurrency] = useState('INR');
+  const [result, setResult] = useState(null);
+
+  useEffect(() => {
+    fetch('https://webappsstatic.paytm.com/growth-api/currency/rate/rate.json?channel=web&child_site_id=1&site_id=1&version=2')
+      .then(response => response.json())
+      .then(data => setData(data));
+  }, []);
+
+  const handleAmountChange = event => {
+    setAmount(event.target.value);
+  };
+
+  const handleFromCurrencyChange = event => {
+    setFromCurrency(event.target.value);
+  };
+
+  const handleToCurrencyChange = event => {
+    setToCurrency(event.target.value);
+  };
+
+  const handleConvert = () => {
+    if (data && amount) {
+      const fromRate = data.rates[fromCurrency];
+      const toRate = data.rates[toCurrency];
+      setResult((amount / fromRate) * toRate);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {data && (
+        <>
+          <input type="number" value={amount} onChange={handleAmountChange}  />
+          <select value={fromCurrency} onChange={handleFromCurrencyChange}>
+            {Object.keys(data.rates).sort().map(currency => (
+              <option key={currency} value={currency}>
+                {currency}
+              </option>
+            ))}
+          </select>
+          <select value={toCurrency} onChange={handleToCurrencyChange}>
+            {Object.keys(data.rates).sort().map(currency => (
+              <option key={currency} value={currency}>
+                {currency}
+              </option>
+            ))}
+          TO
+          </select>
+          <button onClick={handleConvert}>Convert</button>
+          {result && <p>{result}</p>}
+        </>
+      )}
     </div>
   );
 }
